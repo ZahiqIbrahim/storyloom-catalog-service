14b6ea7398d518db449867a0d200e45a23236f91
Zahiq Ibrahim <161963558+ZahiqIbrahim@users.noreply.github.com>
2026-06-17 23:38:21 +0530

    Added Movie service and Movie controller

--- src/main/java/com/example/storyloom_catalog_service/controller/MoviesController.java
+++ src/main/java/com/example/storyloom_catalog_service/controller/MoviesController.java
@@ -1,4 +1,29 @@
 package com.example.storyloom_catalog_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/movies")
 public class MoviesController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping("/getMovie")
+    public ResponseEntity<?> getMovie(@RequestBody String movieTitle){
+        try{
+
+            return ResponseEntity.ok(movieService.getMovie(movieTitle));
+
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(Map.of("Error", e.getMessage()));
+        }
+    }
 }
---
+++ src/main/java/com/example/storyloom_catalog_service/service/MovieService.java
@@ -0,0 +1,7 @@
+package com.example.storyloom_catalog_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+}
